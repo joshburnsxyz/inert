db128604905eccae4ae169d6f4475f3c63669155
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2023-01-03 01:34:25 +0000

    static server middleware

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const Koa = require('koa');
+import Koa from 'koa';
+import serve from 'koa-static';
+import path from "path";
 
 const app = new Koa;
 
-
+// Mount middleware
+app.use(serve(path.join(__dirname)));
 
 // Start server and listen for connections
 app.listen(3000);
