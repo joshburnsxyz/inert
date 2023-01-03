479c3935d0223039ddb4236f0f25a6123c857e8b
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2023-01-03 01:39:44 +0000

    add basic compress middleware

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-import Koa from 'koa';
-import serve from 'koa-static';
-import path from "path";
+const Koa = require("koa")
+const serve = require("koa-static")
+const path = require("path")
+const compress = require("koa-compress")
 
 const app = new Koa;
 
 // Mount middleware
 app.use(serve(path.join(__dirname)));
+app.use(compress({
+  br: true
+}));
 
 // Start server and listen for connections
 app.listen(3000);
