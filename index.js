83812d32a25407046b0b4d949339157605f193a0
Josh Burns <joshyburnss@gmail.com>
2023-01-06 22:18:12 +1100

    server creates public directory if it doesnt exist on boot

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const Koa = require("koa")
 const serve = require("koa-static")
 const path = require("path")
 const compress = require("koa-compress")
+const fs = require("fs")
 
 const app = new Koa;
+const myDir = path.join(__dirname, "./public");
+
+// Ensure public directory exists before booting server
+try{
+     fs.lstatSync(myDir).isDirectory()
+}catch(e){
+   if(e.code == 'ENOENT'){
+       fs.mkdir(myDir, () => {});
+   }
+}
 
 // Mount middleware
-app.use(serve(path.join(__dirname, "./public")));
+app.use(serve(myDir));
 app.use(function(req, res, next){
   console.log(req);
   next();
@@ -20,4 +31,7 @@ app.use(compress({
 }));
 
 // Start server and listen for connections
-app.listen(3000, () => {console.log("Starting Inert - static asset delivery service.")});
+app.listen(3000, () => {
+    console.log("Starting Inert - static asset delivery service.");
+    console.log("SERVER BOUND TO PORT 3000");
+});
