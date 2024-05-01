989648d52b7d73f33217bf88c5f91f7d5b1a8961
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2024-05-01 20:22:13 +0000

    setup flag

--- cmd/inert/main.go
+++ cmd/inert/main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+  "fmt"
   "log"
   "net/http"
+  flag "github.com/spf13/pflag"
 )
 
+var (
+  flagPort int
+  flagCert string
+  flagKey string
+  flagDir string
+)
+
+func init() {
+  flag.IntVarP(&flagPort, "port", "p", 8334, "Port to bind server too")
+  flag.StringVarP(&flagCert, "cert", "c", "cert.pem", "SSL Certificate file")
+  flag.StringVarP(&flagKey, "key", "k", "key.pem", "SSL Kery file")
+  flag.StringVarP(&flagDir, "dir", "d", "./static", "Directory to serve as root")
+}
+
 func main() {
-  log.Fatal(http.ListenAndServeTLS(":8443", "cert.pem", "key.pem", http.FileServer(http.Dir("./static"))))
+  flag.Parse()
+  finalPort := fmt.Sprintf(":%d", flagPort)
+  log.Fatal(http.ListenAndServeTLS(finalPort, flagCert, flagKey, http.FileServer(http.Dir(flagDir))))
 }
