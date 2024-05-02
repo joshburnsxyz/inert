8b7ce6e2e652886563f7cdaf2c9eb0c0e6f66c5f
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2024-05-02 02:59:25 +0000

    document flags and their intentions

--- cmd/inert/main.go
+++ cmd/inert/main.go
@@ -16,10 +16,19 @@ var (
 )
 
 func init() {
+  // --ssl - Enables SSL mode (requires --cert and --key to be present)
   flag.BoolVar(&flagSsl, "ssl", false, "Run the server with SSL support (requires key.pem and cert.pem files)")
+  
+  // --port, -p - Sets server port
   flag.IntVarP(&flagPort, "port", "p", 8334, "Port to bind server too")
+  
+  // --cert, -c - Sets the path to the certificate file to read for SSL mode
   flag.StringVarP(&flagCert, "cert", "c", "cert.pem", "SSL Certificate file")
+
+  // --key, -k - Sets the path to the key file to read for SSL mode
   flag.StringVarP(&flagKey, "key", "k", "key.pem", "SSL Kery file")
+
+  // --dir, -d - Sets the path to the directory that will act as the root directory
   flag.StringVarP(&flagDir, "dir", "d", "./static", "Directory to serve as root")
 }
 
