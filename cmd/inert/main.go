e4f359d061c45baaaa4ed3a7cef100aa1b876e48
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2024-05-02 02:52:36 +0000

    make SSL optional

--- cmd/inert/main.go
+++ cmd/inert/main.go
@@ -9,12 +9,14 @@ import (
 
 var (
   flagPort int
+  flagSsl bool
   flagCert string
   flagKey string
   flagDir string
 )
 
 func init() {
+  flag.BoolVar(&flagSsl, "ssl", false, "Run the server with SSL support (requires key.pem and cert.pem files)")
   flag.IntVarP(&flagPort, "port", "p", 8334, "Port to bind server too")
   flag.StringVarP(&flagCert, "cert", "c", "cert.pem", "SSL Certificate file")
   flag.StringVarP(&flagKey, "key", "k", "key.pem", "SSL Kery file")
@@ -24,5 +26,9 @@ func init() {
 func main() {
   flag.Parse()
   finalPort := fmt.Sprintf(":%d", flagPort)
-  log.Fatal(http.ListenAndServeTLS(finalPort, flagCert, flagKey, http.FileServer(http.Dir(flagDir))))
+  if flagSsl {
+    log.Fatal(http.ListenAndServeTLS(finalPort, flagCert, flagKey, http.FileServer(http.Dir(flagDir))))
+  } else {
+    log.Fatal(http.ListenAndServe(finalPort, http.FileServer(http.Dir(flagDir))))
+  }
 }
