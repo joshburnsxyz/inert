35dd74b80654f7b13160790fbcd6e1e5b10b398d
Josh Burns <joshyburnss@gmail.com>
2024-05-07 18:54:16 +1000

    more UI work

--- cmd/inert/fileserver.go
+++ cmd/inert/fileserver.go
@@ -16,24 +16,42 @@ func makeFS(dir string) (http.HandlerFunc, error) {
 
 	// Build HTML template
 	tmpl, err := template.New("file_index").Parse(`
-<!doctype HTML>
+<!DOCTYPE html>
 <html data-theme="dark">
 <head>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.0/css/bulma.min.css"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Directory Listing</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.0/css/bulma.min.css">
+    <style>
+        body {
+            padding-top: 40px;
+            padding-bottom: 40px;
+        }
+        .container {
+            max-width: 600px;
+            margin: auto;
+        }
+        .error {
+            color: red;
+        }
+    </style>
 </head>
 <body>
-{{if .IsDir}}
-    <div class="container">
-	<ul>
-	    {{range .DirEntries}}
-		<li><a href="{{.Name}}">{{.Name}}</a></li>
-	    {{end}}
-	</ul>
-    </div>
-{{else}}
-<h1>Error</h1>
-<p>{{.Error}}</p>
-{{end}}
+<div class="container">
+    {{if .IsDir}}
+        <h1 class="title">Directory Listing</h1>
+        <ul class="menu-list">
+            {{range .DirEntries}}
+                <li><a href="{{.Name}}" class="has-text-white">{{.Name}}</a></li>
+		<hr/>
+            {{end}}
+        </ul>
+    {{else}}
+        <h1 class="title">Error</h1>
+        <p class="error">{{.Error}}</p>
+    {{end}}
+</div>
 </body>
 </html>
 `)
