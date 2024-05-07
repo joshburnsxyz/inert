f35c8660a83eebda540d7b22c406a4d6cdd02477
Josh Burns <joshyburnss@gmail.com>
2024-05-07 17:32:04 +1000

    fix fileserver mechanics, use inbuilt HTML template

--- cmd/inert/fileserver.go
+++ cmd/inert/fileserver.go
@@ -1,46 +1,113 @@
 package main
 
 import (
-	"errors"
-	"io"
 	"net/http"
+	"html/template"
+	"os"
+	"log"
 )
 
-func makeFS(dir string) (http.HandlerFunc, error) {
-	var (
-		err      error
-		handler  http.HandlerFunc
-		htmlHead string = "<head>\n<link rel=\"stylesheet\" type=\"text/css\" href=\"https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.0/css/bulma.min.css\"/></head>"
-	)
+type fileRecord struct {
+	Name string
+	Size int64
+	Path string
+}
 
+func makeFS(dir string) (http.HandlerFunc, error) {
 	fs := http.FileServer(http.Dir(dir))
 
-	handler = func(w http.ResponseWriter, r *http.Request) {
-		var (
-			url   = r.URL.Path
-			isDir = url[len(url)-1] == '/'
-		)
-
-		if isDir {
-			err = nil
-			io.WriteString(w, "<!doctype HTML>")
-			io.WriteString(w, "\n")
-			io.WriteString(w, "<html data-theme=\"dark\">")
-			io.WriteString(w, "\n")
-			io.WriteString(w, htmlHead)
-			io.WriteString(w, "\n")
-			io.WriteString(w, "<body>")
-			io.WriteString(w, "\n")
-			fs.ServeHTTP(w, r)
-			io.WriteString(w, "\n")
-			io.WriteString(w, "</body>")
-			io.WriteString(w, "\n")
-			io.WriteString(w, "</html>")
-		} else {
-			err = errors.New("Directory not found, could not generate HTTP handler.")
-		}
-
+	// Build HTML template
+	tmpl, err := template.New("file_index").Parse(`
+<!doctype HTML>
+<html data-theme="dark">
+<head>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.0/css/bulma.min.css"/>
+</head>
+<body>
+{{if .IsDir}}
+<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Size</th>
+        </tr>
+    </thead>
+    <tbody>
+        {{range .DirEntries}}
+        <tr>
+            <td><a href="{{.Name}}">{{.Name}}</a></td>
+            <td>{{.Size}}</td>
+        </tr>
+        {{end}}
+    </tbody>
+</table>
+{{else}}
+<h1>Error</h1>
+<p>{{.Error}}</p>
+{{end}}
+</body>
+</html>
+`)
+	if err != nil {
+		return nil,err
 	}
 
-	return handler, err
+	// Build HTTP handler
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		file,err := os.Stat(dir + r.URL.Path)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Build struct to hold data to populate the HTML template with.
+		data := struct {
+			IsDir bool
+			Error error
+			DirEntries []fileRecord
+		}{
+			IsDir: file.IsDir(),
+			Error: nil,
+			DirEntries: nil,
+		}
+
+		if file.IsDir() {
+			data.Error = nil
+			dirEntries, err := os.ReadDir(dir + r.URL.Path)
+			var files []fileRecord
+
+			// Build a custom data struct so we can calculate the absolute path to the file
+			for _,d := range dirEntries {
+				info, _ := d.Info()
+				new_file := fileRecord{
+					Name: d.Name(),
+					Size: info.Size(),
+					Path: d.Name(),
+				}
+				files = append(files, new_file)
+
+			}
+
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			data.DirEntries = files
+			log.Println(data.DirEntries)
+			fs.ServeHTTP(w,r)
+		} else {
+			// FIXME: Serve the raw file instead of directory contents
+			data.Error = os.ErrNotExist
+		}
+
+		// Populate template
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	// Upon success, return the handler and no error
+	return handler,nil
 }
